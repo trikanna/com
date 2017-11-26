6a01735e8c6d0f525f4fae615367205f4eb4fb41
Santiago Camelo <trikanna@gmail.com>
2017-11-26 03:56:13 -0300

    metas

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -25,7 +25,7 @@ const TemplateWrapper = ({ children }) => (
         },
         {
           name: 'viewport',
-          content: 'width=device-width, initial-scale=1.0, user-scalable=no, shrink-to-fit=no'
+          content: 'width=device-width, initial-scale=1.0, user-scalable=no'
         }
       ]}
     />
