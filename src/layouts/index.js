1ba04f2e430c06c146f875df59f96a9cdf382794
Santiago Camelo <trikanna@gmail.com>
2017-11-26 04:25:21 -0300

    maximum-scale

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -25,7 +25,7 @@ const TemplateWrapper = ({ children }) => (
         },
         {
           name: 'viewport',
-          content: 'width=device-width, initial-scale=1.0'
+          content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0'
         }
       ]}
     />
