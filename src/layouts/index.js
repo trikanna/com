23d94e51d7d5b0f4cb46df5824aebf76a43c684f
Santiago Camelo <trikanna@gmail.com>
2017-11-26 04:35:04 -0300

    initial-scale

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -25,7 +25,7 @@ const TemplateWrapper = ({ children }) => (
         },
         {
           name: 'viewport',
-          content: 'width=device-width, initial-scale=1.0, user-scalable=no'
+          content: 'width=device-width, initial-scale=1.0'
         }
       ]}
     />
